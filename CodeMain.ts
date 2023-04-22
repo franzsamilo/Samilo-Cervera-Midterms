85ed1b9309429029687f76b1c1eae2c794ac6588
louiecerv <90561528+cezro@users.noreply.github.com>
2023-04-23 00:53:39 +0800

    Changed drug actions to pharmacy database methods

--- CodeMain.ts
+++ CodeMain.ts
@@ -1,118 +1,133 @@
 interface PharmaceuticalsDatabase {
-    get Price(): number;
-    set Price(pirce: number);
-    get Manufacturer(): string;
-    get ExpiryDate(): Date;
-    set ExpiryDate(expiryDate: Date);
-    get ActiveIngredients(): string[];
-    get BrandName(): string;
-    get GenericName(): string;
-    isPrescriptionRequired(): boolean;
-    isExpired(): boolean;
+  get Stock();
+  set Stock(amount: number);
+  get Price(): number;
+  set Price(price: number);
+  get RetailPrice(): number;
+  set RetailPrice(price: number)
+  get Manufacturer(): string;
+  get ExpiryDate(): Date;
+  set ExpiryDate(expiryDate: Date);
+  get ActiveIngredients(): string[];
+  get BrandName(): string;
+  get GenericName(): string;
+  isPrescriptionRequired(): boolean;
+  isExpired(): boolean;
+}
+
+abstract class Painkillers implements PharmaceuticalsDatabase {
+  private stock: number;
+  private price: number;
+  private retailPrice: number; 
+  private manufacturer: string;
+  private expiryDate: Date;
+  private activeIngredients: string[];
+  private prescriptionRequired: boolean;
+  public brandName: string;
+  public genericName: string;
+  private usageInstructions: string;
+  
+  constructor( price: number, manufacturer: string, expiryDate: Date, activeIngredients: string[], prescriptionRequired: boolean, brandName: string, genericName: string) {
+    this.price = price;
+    this.manufacturer = manufacturer;
+    this.expiryDate = expiryDate;
+    this.activeIngredients = activeIngredients;
+    this.prescriptionRequired = prescriptionRequired;
+    this.brandName = brandName;
+    this.genericName = genericName;
+    this.usageInstructions = "";
+  }
+
+  get Stock(): number {
+    return this.stock;
+  }
+
+  set Stock(amount: number) {
+    this.stock = amount;
+  }
+
+  get Price(): number {
+    return this.price;
   }
   
-  abstract class Painkillers implements PharmaceuticalsDatabase {
-    protected price: number;
-    private manufacturer: string;
-    private expiryDate: Date;
-    private activeIngredients: string[];
-    private prescriptionRequired: boolean;
-    public brandName: string;
-    public genericName: string;
-    protected usageInstructions: string;
-    
-    constructor( price: number, manufacturer: string, expiryDate: Date, activeIngredients: string[], prescriptionRequired: boolean, brandName: string, genericName: string) {
+  set Price(price: number) {
       this.price = price;
-      this.manufacturer = manufacturer;
-      this.expiryDate = expiryDate;
-      this.activeIngredients = activeIngredients;
-      this.prescriptionRequired = prescriptionRequired;
-      this.brandName = brandName;
-      this.genericName = genericName;
-      this.usageInstructions = "";
-    }
-    
-    get Price(): number {
-      return this.price;
-    }
-    
-    set Price(price: number) {
-        this.price = price;
-    }
-    get Manufacturer(): string {
-      return this.manufacturer;
+  }
+
+  get RetailPrice(): number {
+    return this.retailPrice;
+  }
+
+  set RetailPrice(price: number) {
+    this.retailPrice = price;
+  }
+  get Manufacturer(): string {
+    return this.manufacturer;
+  }
+
+  get ExpiryDate(): Date {
+    return this.expiryDate;
+  }
+
+  set ExpiryDate(expiryDate: Date) {
+    this.expiryDate = expiryDate;
+  }
+
+  get ActiveIngredients(): string[] {
+    return this.activeIngredients;
+  }
+
+  get BrandName(): string {
+      return this.brandName;
     }
   
-    get ExpiryDate(): Date {
-      return this.expiryDate;
-    }
-  
-    set ExpiryDate(expiryDate: Date) {
-      this.expiryDate = expiryDate;
-    }
-  
-    get ActiveIngredients(): string[] {
-      return this.activeIngredients;
+  get GenericName(): string {
+      return this.genericName;
     }
 
-    get BrandName(): string {
-        return this.brandName;
-      }
-    
-    get GenericName(): string {
-        return this.genericName;
-      }
-  
-    isPrescriptionRequired(): boolean {
-      return this.prescriptionRequired;
-    }
-  
-    isExpired(): boolean {
-      const currentDate = new Date();
-      return this.expiryDate < currentDate;
-    }
-  
-    public setUsageInstructions(instructions: string): void {
-      this.usageInstructions = instructions;
-    }
-  
-    public getUsageInstructions(): string {
-      return this.usageInstructions;
-    }
-  
-    private checkExpiryDate(): void {
+  isPrescriptionRequired(): boolean {
+    return this.prescriptionRequired;
+  }
+
+  isExpired(): boolean {
+    const currentDate = new Date();
+    return this.expiryDate < currentDate;
+  }
+
+  public setUsageInstructions(instructions: string): void {
+    this.usageInstructions = instructions;
+  }
+
+  public getUsageInstructions(): string {
+    return this.usageInstructions;
+  }
+
+  public sell(amount: number): void {
       if (this.isExpired()) {
         console.log("This medication has expired and should be discarded.");
+      } else {
+        console.log(`A total of ${this.price*amount} ${this.brandName} hass been sold.`);
       }
-    }
-  
-    public openBottle(): void {
-      console.log("The medication bottle has been opened.");
-      this.checkExpiryDate();
-    }
-  
-    public closeBottle(): void {
-      console.log("The medication bottle has been closed.");
-      this.checkExpiryDate();
-    }
-  
-    public swallowPill(): void {
-      console.log("The pill has been swallowed.");
-      this.checkExpiryDate();
-    }
-  
-    public crushTablet(): void {
-      console.log("The tablet has been crushed.");
-      this.checkExpiryDate();
-    }
-  
-    public dissolvePowder(): void {
-      console.log("The powder has been dissolved.");
-      this.checkExpiryDate();
-    }
-  
-    public abstract treatPain(): void;
-    public abstract get medicationSideEffects(): string[];
-    public abstract uniqueEffects(): string[];
   }
+
+  public discardMedication(): void {
+    if (this.isExpired()) {
+      console.log(`${this.brandName} has been discarded due to expiration.`);
+    } else {
+      console.log(`${this.brandName} has been discarded.`);
+    }
+  }
+
+  public restock(amount: number): void {
+    this.stock += amount;
+    console.log(`Restocked ${amount} of ${this.brandName} from ${this.manufacturer}.`);
+  }
+
+  public abstract treatPain(): void;
+  public abstract get medicationSideEffects(): string[];
+  public abstract uniqueEffects(): string[];
+}
+
+
   
+
